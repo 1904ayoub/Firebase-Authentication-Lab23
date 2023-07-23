a8fa8ac7ce15df6321fa4ec51fa81b0ca1eba386
AyoubAy24-meet <129674378+1904ayoub@users.noreply.github.com>
2023-07-23 18:50:17 +0300

    everything :)
    suiiiiiiiiiii

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
-firebaseConfig = {
+config = {
   "apiKey": "AIzaSyDwdBIKm59pWBEFZZBe1Em0MtVjx9IcdT0",
   "authDomain": "wardabdotheg.firebaseapp.com",
   "projectId": "wardabdotheg",
@@ -15,34 +15,33 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
-
-
 @app.route('/', methods=['GET', 'POST'])
+def signup():
+   Error = ""
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('signin'))
+       except:
+            Error = "Authentication failed"
+   return render_template("signup.html",Error=Error)
+
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
    Error = ""
    if request.method == 'POST':
-       Email = request.form['email']
-       Password = request.form['password']
+       email = request.form['email']
+       password = request.form['password']
        try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('/add_tweet'))
+            return redirect(url_for('add_tweet'))
        except:
             Error = "Authentication failed"
    return render_template("signin.html",Error=Error)
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-   Error = ""
-   if request.method == 'POST':
-       Email = request.form['email']
-       Password = request.form['password']
-       try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('/signup'))
-       except:
-            Error = "Authentication failed"
-   return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
